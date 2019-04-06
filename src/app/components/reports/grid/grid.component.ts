af6b3360742b4ff0fa46a4f739a324442ed7ebbe
Manuel Colorado <manuel1412@gmail.com>
2019-04-06 23:05:59 +0100

    Added FAB button to add tasks. Fixed chart grid.

--- src/app/components/reports/grid/grid.component.ts
+++ src/app/components/reports/grid/grid.component.ts
@@ -8,27 +8,34 @@ import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
   styleUrls: ['./grid.component.scss']
 })
 export class GridComponent {
+  public cards = [
+    { title: 'Chart 1', cols: 2, rows: 1, visible: true },
+    { title: 'Chart 2', cols: 2, rows: 1, visible: true },
+    { title: 'Chart 3', cols: 2, rows: 1, visible: true },
+    { title: 'Chart 4', cols: 2, rows: 1, visible: true }
+  ];
+
   /** Based on the screen size, switch from standard to one column per row */
-  cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
-    map(({ matches }) => {
-      // For smaller screens, make charts to fill 2 cols, 1 row making a single column of charts
-      if (matches) {
-        return [
-          { title: 'Chart 1', cols: 2, rows: 1 },
-          { title: 'Chart 2', cols: 2, rows: 1 },
-          { title: 'Chart 3', cols: 2, rows: 1 },
-          { title: 'Chart 4', cols: 2, rows: 1 }
-        ];
-      }
-      // Bigger screens will display 2x2 grid of charts
-      return [
-        { title: 'Chart 1', cols: 1, rows: 1 },
-        { title: 'Chart 2', cols: 1, rows: 1 },
-        { title: 'Chart 3', cols: 1, rows: 1 },
-        { title: 'Chart 4', cols: 1, rows: 1 }
-      ];
-    })
-  );
+  // cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
+  //   map(({ matches }) => {
+  //     // For smaller screens, make charts to fill 2 cols, 1 row making a single column of charts
+  //     if (matches) {
+  //       return [
+  //         { title: 'Chart 1', cols: 2, rows: 1, visible: true },
+  //         { title: 'Chart 2', cols: 2, rows: 1, visible: true },
+  //         { title: 'Chart 3', cols: 2, rows: 1, visible: true },
+  //         { title: 'Chart 4', cols: 2, rows: 1, visible: true }
+  //       ];
+  //     }
+  //     // Bigger screens will display 2x2 grid of charts
+  //     return [
+  //       { title: 'Chart 1', cols: 1, rows: 1, visible: true },
+  //       { title: 'Chart 2', cols: 1, rows: 1, visible: true },
+  //       { title: 'Chart 3', cols: 1, rows: 1, visible: true },
+  //       { title: 'Chart 4', cols: 1, rows: 1, visible: true }
+  //     ];
+  //   })
+  // );
 
   constructor(private breakpointObserver: BreakpointObserver) {}
 }
